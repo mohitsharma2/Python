7caa70705790fb8b2514f623efa3de41ae732fdd
mohitsharma2 <mohit121993@gmail.com>
2019-12-03 18:32:45 +0530

    added new file

---
+++ style1.py
@@ -0,0 +1,4 @@
+name=input('enter your name:')
+print('name in uppercase character:',name.upper())
+print('name in lowercase character:',name.lower())
+print('name in Titlecase character:',name.title())
