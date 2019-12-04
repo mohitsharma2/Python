da63d2e5904c9a66825b8b573b7c6657ac3dac05
mohitsharma2 <mohit121993@gmail.com>
2019-12-04 17:58:57 +0530

    added new file

---
+++ hands on 2.py
@@ -0,0 +1,133 @@
+#'''Hands On'''
+
+'''
+string = "Rajasthan"
+#Print characters at the even index number 
+
+input='Rajasthan'
+print(input[ : :2])
+                   '''
+
+
+
+
+
+'''
+string = "Rajasthan"
+#Print the given string in reverse
+
+print(input[-1:-10:-1])
+                      '''
+
+
+
+
+
+'''
+string = "Forsk Technologies"
+#Print Forsk using slicing ( forward indexing Left to Right  )
+
+input="Forsk Technologies"
+
+print(input[0:5])
+                 '''
+
+
+
+
+
+
+'''
+string = "Forsk Technologies"
+#Print Technologies using slicing ( forward indexing Left to Right )
+
+input="Forsk Technologies"
+
+print(input[6:18])
+                  '''
+
+
+
+
+
+
+'''
+string = "Forsk Technologies"
+#Print Forsk using slicing ( Reverse indexing Right to Left  ) 
+
+input="Forsk Technologies"
+
+print(input[-18:-13])
+                     '''  
+
+
+
+
+
+'''
+string = "Forsk Technologies"
+#Print Technologies using slicing ( forward indexing Right to Left )
+
+
+input="Forsk Technologies"
+
+print(input[-12:])
+                  '''
+
+
+
+
+
+'''
+string = "Forsk Technologies"
+#Print ksroF using slicing ( forward indexing and Reverse  Indexing)
+
+input="Forsk Technologies"
+
+print(input[-14:-19:-1])
+print(input[4::-1])
+                   '''
+
+
+
+
+
+'''
+string = "Forsk Technologies"
+#Print siesgolonhceT using slicing ( forward indexing Left to Right )
+
+input="Forsk Technologies"
+
+print(input[-1:-13:-1])
+                       '''
+
+
+
+
+
+'''
+string = "Forsk Technologies"
+#Print Technologies Forsk using slicing ( forward indexing Left to Right  )
+
+input="Forsk Technologies"
+
+print(input[6:],input[0:5])
+                           '''
+
+
+
+
+
+
+'''
+string = "Forsk Technologies"
+#Print Technologies Forsk using slicing ( Reverse indexing Right to Left ) 
+
+input="Forsk Technologies"
+
+print(input[-12:],input[-18:-13])
+                                 '''
+
+
+
+
