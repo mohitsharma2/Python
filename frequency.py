a1dcf0d87f4844dcb3565e00b8e27c5df36a0c34
mohitsharma2 <mohit121993@gmail.com>
2019-12-17 16:11:15 +0530

    added new file

---
+++ frequency.py
@@ -0,0 +1,79 @@
+"""
+Name: 
+    Character Frequency       
+Filename:
+    frequency.py 
+Problem Statement:
+    This program accepts a string from User and counts the number of characters 
+    (character frequency) in the input string. 
+Data:
+    Not required
+Extension:
+    Not Available   
+Hint: 
+    Not Available 
+Algorithm:
+    Not Available  
+Boiler Plate Code:
+    Not Available 
+Sample Input:
+    www.google.com
+Sample Output:
+    {'c': 1, 'e': 1, 'g': 2, 'm': 1, 'l': 1, 'o': 3, '.': 2, 'w': 3}
+"""
+
+'''
+dict1={}
+inp=input('Enter the word: ')
+for char in inp:
+    if char not in dict1:
+        dict1[char]=1
+    else:
+        dict1[char]=dict1[char]+1
+print(dict1)     
+for i,j in dict1.items():    # for showing in different line
+    print(i,j)
+
+
+ output--------------------------------------------------------------------   
+Enter the word: www.google.com
+{'w': 3, '.': 2, 'g': 2, 'o': 3, 'l': 1, 'e': 1, 'c': 1, 'm': 1}
+w 3
+. 2
+g 2
+o 3
+l 1
+e 1
+c 1
+m 1
+
+----------------------------------------------------------------------------
+
+
+str1 = "Mohit SharMa"
+
+dict1 = {}
+
+for char in str1:
+    if char not in dict1:
+        dict1[char]  = 1
+    else:
+        dict1[char] = dict1[char] + 1
+print(dict1)
+
+output========================================================================s
+{'M': 2, 'o': 1, 'h': 2, 'i': 1, 't': 1, ' ': 1, 'S': 1, 'a': 2, 'r': 1}
+
+
+dict1={}
+
+str1='chotu sharma'
+for char in str1:
+    if char not in dict1:
+        dict1[char]=1
+    else:
+        dict1[char]=dict1[char]+1
+print(dict1)
+for i,j in dict1.items():
+    print(i,j)
+'''
