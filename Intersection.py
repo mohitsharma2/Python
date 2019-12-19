c4dcb7f19f46ea2abe0672251265f71930e9d507
mohitsharma2 <mohit121993@gmail.com>
2019-12-19 12:27:54 +0530

    added new file

---
+++ Intersection.py
@@ -0,0 +1,31 @@
+"""
+Name: 
+    Intersection       
+Filename:
+    Intersection.py 
+Problem Statement:
+    With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155]
+    Write a program to make a list whose elements are intersection of the above given lists. 
+Data:
+    Not required
+Extension:
+    Not Available   
+Hint: 
+    Not Available 
+Algorithm:
+    Not Available  
+Boiler Plate Code:
+    Not Available 
+Sample Input:
+    Not Available
+Sample Output:
+    Not Available
+""" 
+list1=[1,3,6,7,35,55]
+list2=[12,24,35,24,88,120,155]
+
+for i in list1:
+    if i in list2:
+        print(i)
+    else:
+      pass
