fdf47e970b220ead4b22a36befd272c3c8d15d2a
mohitsharma2 <mohit121993@gmail.com>
2019-12-23 16:15:54 +0530

    added new file

---
+++ reverse.py
@@ -0,0 +1,37 @@
+"""
+Name: 
+    Reverse Function         
+Filename:
+    reverse.py
+Problem Statement:
+    Define a function reverse() that computes the reversal of a string.
+    Without using Python's inbuilt function
+    Take input from User
+Data:
+    Not required
+Extension:
+    Not Available  
+Hint: 
+    Not Available 
+Algorithm:
+    Not Available  
+Boiler Plate Code:
+    Not Available 
+Sample Input:
+    I am testing
+Sample Output:
+    gnitset ma I
+"""
+
+inp=input('Enter the string:')
+inp=inp[::-1]
+def reverse(inp):    
+    return(inp)
+print(inp)
+
+'''
+output=================================================================
+
+Enter the string:I am testing
+gnitset ma I
+'''
