f09cae053e1e62dd62464ab635b7a671f2f2bee1
mohitsharma2 <mohit121993@gmail.com>
2020-01-04 22:02:21 +0530

    Added new file

---
+++ name_english.py
@@ -0,0 +1,17 @@
+#Name Printing in English
+
+first_name=input('enter your first name:')
+last_name=input('enter your last name:')
+
+print('"'+first_name+'"'+"\n"+'"'+last_name+'"')
+
+
+
+'''
+output==========================================
+
+enter your first name:Mohit
+enter your last name:Sharma
+"Mohit"
+"Sharma"
+'''
