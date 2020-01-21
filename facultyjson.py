9468f345b576954aa5021e5c835136eaabd02ee2
mohitsharma2 <mohit121993@gmail.com>
2020-01-21 11:59:42 +0530

    Added new file

---
+++ facultyjson.py
@@ -0,0 +1,27 @@
+import json
+[{
+	"Faculty1": [{
+		"First Name": "Om prakash",
+		"Last Name": "Sharma",
+		"Photo": " http://tineye.com/images/op.jpg.",
+		"Department": "Computer department",
+		"Research Areas": "Deep Learning",
+		"Contact Detail": [{
+			"Phone Number": "9885215654",
+			"Email id": "omprakash@gmail.com"
+		}]
+	}]
+}, {
+	"Faculty2": [{
+		"First Name": "Suraj",
+		"Last Name": "Sharma",
+		"Photo": " http://tineye.com/images/suraj.jpg.",
+		"Department": "Electrical department",
+		"Research Areas": "Inductance",
+		"Contact Detail": [{
+			"Phone Number": "9785255654",
+			"Email id": "suraj12@gmail.com"
+		}]
+
+	}]
+}]
