812074d62c57991cb82b2f04e9fce5fdea4df309
mohitsharma2 <mohit121993@gmail.com>
2020-01-30 23:30:35 +0530

    Added new file

--- studentjson.py
+++ studentjson.py
@@ -23,6 +23,8 @@ Code Challenge
 
 
 import json
+
+jso="""
 {
 	"details": {
 		"Faculty1": {
@@ -50,7 +52,141 @@ import json
 		}
 	}
 }
-print(type())
+ """
+my=json.loads(jso)
+print(my)
+type(my)
+me=json.dumps(my)
+print(me)
+type(me)
+with open("C:/Users/mohit/Desktop/studennn.json", "w") as write_file:
+    json.dump(jso, write_file)
+
+with open("C:/Users/mohit/Desktop/studennn.json", "r") as read_file:
+    j=json.load(read_file)
+    print(j)
+ 
+"""
+output===================================================================
+
+
+my=json.loads(jso)
+
+print(my)
+{'details': {'Faculty1': {'First Name': 'Om prakash', 'Last Name': 'Sharma', 'Photo': ' http://tineye.com/images/op.jpg.', 'Department': 'Computer department', 'Research Areas': 'Deep Learning', 'Contact Detail': [{'Phone Number': '9885215654', 'Email id': 'omprakash@gmail.com'}]}, 'Faculty2': {'First Name': 'Suraj', 'Last Name': 'Sharma', 'Photo': ' http://tineye.com/images/suraj.jpg.', 'Department': 'Electrical department', 'Research Areas': 'Inductance', 'Contact Detail': [{'Phone Number': '9785255654', 'Email id': 'suraj12@gmail.com'}]}}}
+
+me=json.dumps(my)
+
+print(me)
+{"details": {"Faculty1": {"First Name": "Om prakash", "Last Name": "Sharma", "Photo": " http://tineye.com/images/op.jpg.", "Department": "Computer department", "Research Areas": "Deep Learning", "Contact Detail": [{"Phone Number": "9885215654", "Email id": "omprakash@gmail.com"}]}, "Faculty2": {"First Name": "Suraj", "Last Name": "Sharma", "Photo": " http://tineye.com/images/suraj.jpg.", "Department": "Electrical department", "Research Areas": "Inductance", "Contact Detail": [{"Phone Number": "9785255654", "Email id": "suraj12@gmail.com"}]}}}
+
+my=json.loads(jso)
+print(my)
+type(my)
+me=json.dumps(my)
+print(me)
+type(me)
+{'details': {'Faculty1': {'First Name': 'Om prakash', 'Last Name': 'Sharma', 'Photo': ' http://tineye.com/images/op.jpg.', 'Department': 'Computer department', 'Research Areas': 'Deep Learning', 'Contact Detail': [{'Phone Number': '9885215654', 'Email id': 'omprakash@gmail.com'}]}, 'Faculty2': {'First Name': 'Suraj', 'Last Name': 'Sharma', 'Photo': ' http://tineye.com/images/suraj.jpg.', 'Department': 'Electrical department', 'Research Areas': 'Inductance', 'Contact Detail': [{'Phone Number': '9785255654', 'Email id': 'suraj12@gmail.com'}]}}}
+{"details": {"Faculty1": {"First Name": "Om prakash", "Last Name": "Sharma", "Photo": " http://tineye.com/images/op.jpg.", "Department": "Computer department", "Research Areas": "Deep Learning", "Contact Detail": [{"Phone Number": "9885215654", "Email id": "omprakash@gmail.com"}]}, "Faculty2": {"First Name": "Suraj", "Last Name": "Sharma", "Photo": " http://tineye.com/images/suraj.jpg.", "Department": "Electrical department", "Research Areas": "Inductance", "Contact Detail": [{"Phone Number": "9785255654", "Email id": "suraj12@gmail.com"}]}}}
+Out[27]: str
+
+type(my)
+Out[28]: dict
+
+with open("C:/Users/mohit/Desktop/studennn.json", "w") as write_file:
+    json.dump(jso, write_file)
+
+with open("C:/Users/mohit/Desktop/studennn.json", "w") as write_file:
+    json.dump(jso, write_file)
+
+with open("C:/Users/mohit/Desktop/studennn.json", "r") as read_file:
+    jso=json.load(read_file)
+    print(my)
+{'details': {'Faculty1': {'First Name': 'Om prakash', 'Last Name': 'Sharma', 'Photo': ' http://tineye.com/images/op.jpg.', 'Department': 'Computer department', 'Research Areas': 'Deep Learning', 'Contact Detail': [{'Phone Number': '9885215654', 'Email id': 'omprakash@gmail.com'}]}, 'Faculty2': {'First Name': 'Suraj', 'Last Name': 'Sharma', 'Photo': ' http://tineye.com/images/suraj.jpg.', 'Department': 'Electrical department', 'Research Areas': 'Inductance', 'Contact Detail': [{'Phone Number': '9785255654', 'Email id': 'suraj12@gmail.com'}]}}}
+
+with open("C:/Users/mohit/Desktop/studennn.json", "r") as read_file:
+    jso=json.load(read_file)
+    print(me)
+{"details": {"Faculty1": {"First Name": "Om prakash", "Last Name": "Sharma", "Photo": " http://tineye.com/images/op.jpg.", "Department": "Computer department", "Research Areas": "Deep Learning", "Contact Detail": [{"Phone Number": "9885215654", "Email id": "omprakash@gmail.com"}]}, "Faculty2": {"First Name": "Suraj", "Last Name": "Sharma", "Photo": " http://tineye.com/images/suraj.jpg.", "Department": "Electrical department", "Research Areas": "Inductance", "Contact Detail": [{"Phone Number": "9785255654", "Email id": "suraj12@gmail.com"}]}}}
+
+with open("C:/Users/mohit/Desktop/studennn.json", "r") as read_file:
+    j=json.load(read_file)
+    print(j)
+
+{
+        "details": {
+                "Faculty1": {
+                        "First Name": "Om prakash",
+                        "Last Name": "Sharma",
+                        "Photo": " http://tineye.com/images/op.jpg.",
+                        "Department": "Computer department",
+                        "Research Areas": "Deep Learning",
+                        "Contact Detail": [{
+                                "Phone Number": "9885215654",
+                                "Email id": "omprakash@gmail.com"
+                        }]
+                },
+                "Faculty2": {
+                        "First Name": "Suraj",
+                        "Last Name": "Sharma",
+                        "Photo": " http://tineye.com/images/suraj.jpg.",
+                        "Department": "Electrical department",
+                        "Research Areas": "Inductance",
+                        "Contact Detail": [{
+                                "Phone Number": "9785255654",
+                                "Email id": "suraj12@gmail.com"
+                        }]
+  
+                }
+        }
+}
+ 
+
+my=json.loads(jso)
+print(my)
+type(my)
+me=json.dumps(my)
+print(me)
+type(me)
+with open("C:/Users/mohit/Desktop/studennn.json", "w") as write_file:
+    json.dump(jso, write_file)
+
+
+with open("C:/Users/mohit/Desktop/studennn.json", "r") as read_file:
+    j=json.load(read_file)
+    print(j)
+{'details': {'Faculty1': {'First Name': 'Om prakash', 'Last Name': 'Sharma', 'Photo': ' http://tineye.com/images/op.jpg.', 'Department': 'Computer department', 'Research Areas': 'Deep Learning', 'Contact Detail': [{'Phone Number': '9885215654', 'Email id': 'omprakash@gmail.com'}]}, 'Faculty2': {'First Name': 'Suraj', 'Last Name': 'Sharma', 'Photo': ' http://tineye.com/images/suraj.jpg.', 'Department': 'Electrical department', 'Research Areas': 'Inductance', 'Contact Detail': [{'Phone Number': '9785255654', 'Email id': 'suraj12@gmail.com'}]}}}
+{"details": {"Faculty1": {"First Name": "Om prakash", "Last Name": "Sharma", "Photo": " http://tineye.com/images/op.jpg.", "Department": "Computer department", "Research Areas": "Deep Learning", "Contact Detail": [{"Phone Number": "9885215654", "Email id": "omprakash@gmail.com"}]}, "Faculty2": {"First Name": "Suraj", "Last Name": "Sharma", "Photo": " http://tineye.com/images/suraj.jpg.", "Department": "Electrical department", "Research Areas": "Inductance", "Contact Detail": [{"Phone Number": "9785255654", "Email id": "suraj12@gmail.com"}]}}}
+
+{
+        "details": {
+                "Faculty1": {
+                        "First Name": "Om prakash",
+                        "Last Name": "Sharma",
+                        "Photo": " http://tineye.com/images/op.jpg.",
+                        "Department": "Computer department",
+                        "Research Areas": "Deep Learning",
+                        "Contact Detail": [{
+                                "Phone Number": "9885215654",
+                                "Email id": "omprakash@gmail.com"
+                        }]
+                },
+                "Faculty2": {
+                        "First Name": "Suraj",
+                        "Last Name": "Sharma",
+                        "Photo": " http://tineye.com/images/suraj.jpg.",
+                        "Department": "Electrical department",
+                        "Research Areas": "Inductance",
+                        "Contact Detail": [{
+                                "Phone Number": "9785255654",
+                                "Email id": "suraj12@gmail.com"
+                        }]
+  
+                }
+        }
+}
+"""
+
 
 or==================or=====================================
 
@@ -80,3 +216,43 @@ or==================or=====================================
 
 	}]
 }]
+            
+            
+            
+            
+            
+{
+	"Faculty1": {
+		"First Name": "Om prakash",
+		"Last Name": "Sharma",
+		"Photo": " http://tineye.com/images/op.jpg.",
+		"Department": "Computer department",
+		"Research Areas": "Deep Learning",
+		"Contact Detail": [{
+			"Phone Number": "9885215654",
+			"Email id": "omprakash@gmail.com"
+		}]
+	},
+	"Faculty2": {
+		"First Name": "Suraj",
+		"Last Name": "Sharma",
+		"Photo": " http://tineye.com/images/suraj.jpg.",
+		"Department": "Electrical department",
+		"Research Areas": "Inductance",
+		"Contact Detail": {
+			"Phone Number": "9785255654",
+			"Email id": "suraj12@gmail.com"
+		}
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
