e7f916468bcf56122c610ed0bd168ea411a30f5a
mohitsharma2 <mohit121993@gmail.com>
2020-02-02 21:51:41 +0530

    Added new file

--- numbergame_miniproject.py
+++ numbergame_miniproject.py
@@ -5,4 +5,9 @@ guess_number=int(input("Enter the Guess number:"))
 if guess_number==secret_number:
     print("Congratulation You WIN and Computer loose")
 else:
-    print("I'am sorry! You Loose and Computer Wins")
+    print("I'am sorry! You Loose and Computer Wins.Your Guess_number is:{} and secret_number is:{}".format(guess_number,secret_number))
+
+output======================================================
+
+Enter the Guess number:6
+I'am sorry! You Loose and Computer Wins.Your Guess_number is:6 and secret_number is:7
