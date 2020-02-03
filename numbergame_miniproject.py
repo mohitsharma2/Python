a77a254427189927c5ebb664e74e055f547e870a
mohitsharma2 <mohit121993@gmail.com>
2020-02-03 10:56:15 +0530

    Added new file

--- numbergame_miniproject.py
+++ numbergame_miniproject.py
@@ -1,3 +1,17 @@
+"""
+Challenge 1
+    The computer will think of a random number from 1 to 10 as secret number
+    Then ask you ( Player ) to guess the number and store as guess number
+
+    Compare the guess number with the secret number 
+    
+    If the player guesses the right number he wins, 
+    so print player wins and computer lose.
+    
+    If the player guesses the wrong number, 
+    then he loses so print player lose and computer wins.
+"""
+
 import random
 secret_number=random.randint(1,10)
 guess_number=int(input("Enter the Guess number:"))
