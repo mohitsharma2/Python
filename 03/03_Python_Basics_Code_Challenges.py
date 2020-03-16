1640d76c4bd83714b9a0c9d43087554b35c3712d
mohitsharma2 <mohit121993@gmail.com>
2020-03-16 12:52:12 +0530

    Added new folder

---
+++ 03/03_Python_Basics_Code_Challenges.py
@@ -0,0 +1,532 @@
+
+"""
+Name: 
+        
+Filename:
+
+Problem Statement:
+
+Data:
+    Not required
+Extension:
+    Not Available 
+Hint: 
+Algorithm:
+    Not Available  
+Boiler Plate Code:
+    Not Available 
+Sample Input:
+    Not Available 
+Sample Output:
+    Not Available 
+"""
+
+
+"""
+
+Code Challenge
+  Name: 
+    Name Printing in English
+  Filename: 
+    name_english.py
+  Problem Statement:
+    Print your First Name and Last Name in Quotation. 
+    Both the first name and Last name should be on different lines 
+  Hint: 
+     Use the Escape Code for quotation and new line
+     Use + operator for string concatenation
+  Input:
+     Take input from keyboard
+  Output:
+    "Sylvester"
+    "Fernandes"
+
+"""
+
+
+"""
+
+Code Challenge
+  Name: 
+    Temperature of City
+  Filename: 
+    city_temp.py
+  Problem Statement:
+    Print the temperature of your city in Degree Celsius for the day  
+  Hint: 
+     Use \u00b0 as the unicode for Degree 
+  Input:
+     Take input from keyboard
+  Output:
+    26° C
+
+"""
+
+
+
+"""
+Code Challenge
+  Name: 
+    Unicode Printing
+  Filename: 
+    unicode_print.py
+  Problem Statement:
+    Print the below string as it is   
+    UNIX® and Sun Microsystem™ are Copyright ©, 2019 Oracle
+  Hint: 
+    Use unicode \u00AE for Registered ® 
+    Use unicode \u00A9 for Copyright © 
+    Use unicode \u2122 for TradeMark ™ 
+  Input:
+    No input required
+  Output:
+    UNIX® and Sun Microsystem™ are Copyright ©, 2019 Oracle
+"""
+
+
+"""
+
+Code Challenge
+  Name: 
+    Name Printing in Hindi
+  Filename: 
+    name_hindi.py
+  Problem Statement:
+    Print your First Name and Last Name in Devanagari Script ( Hindi ) 
+    with a comma in between   
+  Hint: 
+    http://jrgraphix.net/r/Unicode/0900-097F
+  Input:
+    No input required
+  Output:
+        सिल्वेस्टर , फर्नांडीस
+
+"""
+
+
+
+
+"""
+Code Challenge
+  Name: 
+    Name Printing Checkerboard pattern
+  Filename: 
+    checker.py
+  Problem Statement:
+    Print the checkerboard pattern using escape Codes and Unicode 
+    with multiple print statements and the multiplication operator 
+  Hint: 
+    Eight characters sequence in a line and 
+    the next line should start with a space
+    try to use the * operator for printing
+  Input:
+    No input required
+  Output:
+    * * * * * * * *
+     * * * * * * * *
+    * * * * * * * *
+     * * * * * * * *
+    * * * * * * * *
+     * * * * * * * *
+    * * * * * * * *
+
+"""
+
+"""
+Code Challenge
+  Name: 
+    BMI Help Screen
+  Filename: 
+    bmi_help.py
+  Problem Statement:
+    Print the BMI Value Help Screen   
+  Hint: 
+     Use triple quotes syntax for formatted text
+  Input:
+    No input required
+  Output:
+    World Health Organisation's BMI VALUES ( 8 Levels )
+      Severe Thinness: less than 16 
+      Moderate Thinness: between 16 and 16.9 
+      Mild Thinness: between 17 and 18.4 
+      Normal: between 18.5 and 24.9 
+      Overweight: between 25 and 29.9        
+      Obese Class I: between 30 and 34.9 
+      Obese Class II: between 35 and 39.9 
+      Obese Class III: 40 or greater 
+"""
+
+
+"""
+Code Challenge
+  Name: 
+    BMI in Hindi
+  Filename: 
+    bmi_cal_hindi.py
+  Problem Statement:
+    Convert the BMI program to use hindi titles while taking input and print weight, 
+    height and BMI in Hindi script using formatted strings concept   
+  Hint: 
+     Create a copy of the old bmi_cal.py program and do modification
+"""
+
+
+"""
+Code Challenge
+  Name: 
+    Formatted String
+  Filename: 
+    format.py
+  Problem Statement:
+    This program prints out the following string in a specific format (see the output). 
+  Hint: 
+    The string should be printed using a single print statement only and 
+    the indexes shouldn't be counted manually.
+  Input:
+
+    This is a multi line string. This code challenge is to
+    test your understanding about strings.
+    You need to print some part of this string.
+    From here print this text without manually counting the indexes.
+  Output:
+    From here print this text without manually counting the indexes.
+"""
+
+'''########################################################'''
+'''########################################################'''
+'''########################################################'''
+
+
+'''Hands On'''
+# Generate floating random numbers from interval [3,7)
+
+
+'''Hands On'''
+# Simulate the dice, random number between 1 and 6
+
+
+'''Hands On'''
+# Random element from a list of rock, paper and scissors
+
+
+
+"""
+Code Challenge
+  Name: 
+    Factorial
+  Filename: 
+    factorial.py
+  Problem Statement:
+    Find the factorial of a number. 
+  Hint: 
+    Factorial of 3 = 3 * 2 * 1= 6 
+    Try to first find the function from math module using dir and help 
+  Input:
+    Take the number from the keyboard as input from the user.
+"""
+
+
+"""
+Code Challenge
+  Name: 
+    Area and Perimeter of Circle
+  Filename: 
+    circle.py
+  Problem Statement:
+    Take the radius of the circle from the keyboard as input from the user 
+    Calculate the area and perimeter of it.
+  Hint: 
+    Pi * radius * radius is the area of circle
+    2 * Pi * radius is the perimeter of circle 
+    Use math module to get the value of Pi ( 3.14 ) 
+  Input:
+    Take the radius from the keyboard as input from the user.
+"""
+
+'''########################################################'''
+'''########################################################'''
+'''########################################################'''
+
+'''Hands On'''
+string = "Rajasthan"
+#Print characters at the even index number 
+
+'''Hands On'''
+string = "Rajasthan"
+#Print the given string in reverse 
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print Forsk using slicing ( forward indexing Left to Right  )  
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print Technologies using slicing ( forward indexing Left to Right )  
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print Forsk using slicing ( Reverse indexing Right to Left  )  
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print Technologies using slicing ( forward indexing Right to Left )  
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print ksroF using slicing ( forward indexing and Reverse  Indexing)  
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print siesgolonhceT using slicing ( forward indexing Left to Right )  
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print Technologies Forsk using slicing ( forward indexing Left to Right  )  
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print Technologies Forsk using slicing ( Reverse indexing Right to Left ) 
+
+'''Hands On'''
+# Given strings a and b, return a single string with a and b separated
+# by a space '<a> <b>', except swap the first 2 chars of each string.
+# e.g.
+#   'mix', pod' -> 'pox mid'
+#   'dog', 'dinner' -> 'dig donner'
+# Assume a and b are length 2 or more.
+
+
+
+'''########################################################'''
+'''########################################################'''
+'''########################################################'''
+
+'''Hands On''' 
+# Write a program to count the number of words in a sentence.
+
+
+'''Hands On'''
+# Develop a program that will display a sentence backwards after entered.
+
+'''Hands On'''
+# Given a string s, print the string again
+# where all occurences of its first char have
+# been changed to '*', except do not change
+# the first char itself.
+# e.g. 'babble' yields 'ba**le'
+# Assume that the string is length 1 or more.
+# Hint: try using replace function
+
+
+
+'''Hands On'''
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+# So 'This dinner is not that bad!' yields:
+# This dinner is good!
+
+
+'''Hands On'''
+# Clean the Messy salary into integers for Data Processing
+salary = '$876,001' 
+
+
+
+"""
+Code Challenge
+  Name: 
+    Styling of String
+  Filename: 
+    style.py
+  Problem Statement:
+    Convert to uppercase character
+    Convert to lower character 
+    Convert to CamelCase or TitleCase.
+  Hint: 
+    Try to find some function in the str class and see its help on how to use it.
+    Using dir and help functions
+  Input:
+    Take the name as input from the keyboard. ( SyLvEsTeR )
+"""
+
+
+"""
+Code Challenge
+  Name: 
+    Replacing of Characters
+  Filename: 
+    restart.py
+  Problem Statement:
+    In a hardcoded string RESTART, replace all the R with $ except the first occurrence and print it.
+  Input:
+    RESTART
+  Output: 
+    RESTA$T
+"""
+
+
+"""
+Code Challenge
+  Name: 
+    String Handling
+  Filename: 
+    stringhand.py
+  Problem Statement:
+    Take first and last name in single command from the user and print  
+    them in reverse order with a space between them, 
+    find the index using find/index function and then print using slicing concept of the index
+  Input:
+      Sylvester Fernandes
+  Output: 
+      Fernandes Sylvester 
+"""
+
+
+"""
+Code Challenge
+  Name: 
+    Formatted String
+  Filename: 
+    format2.py
+  Problem Statement:
+    Write a program to print the output in the given format. 
+    Take input from the user. 
+  Hint:
+    Try to serach for some function in the str class using help() and dir()
+  Input:
+    Welcome to Pink City Jaipur
+  Output:
+    Welcome*to*Pink*City*Jaipur
+"""
+
+
+"""
+Code Challenge
+  Name: 
+    Formatted String
+  Filename: 
+    format3.py
+  Problem Statement:
+    Write a program to print the output in the given format. 
+    Take input from the user. 
+  Hint:
+    Try to serach for some function in the str class using help() and dir()
+  Input:
+    Welcome to Pink City Jaipur
+  Output:
+    W*e*l*c*o*m*e* *t*o* *P*i*n*k* *C*i*t*y* *J*a*i*p*u*r
+"""
+
+
+
+"""
+Code Challenge
+  Name: 
+    Formatted String
+  Filename: 
+    format4.py
+  Problem Statement:
+    This program accepts the user's first name and last name 
+    Print them in reverse order with a space between them.
+    Take Input from User 
+    Your code should have only a single user input statement.  
+  Hint:
+    Try to serach for some function in the str using help() and dir()
+  Input:
+    Forsk Technologies
+  Output:
+    Technologies Forsk
+"""
+
+
+'''########################################################'''
+'''########################################################'''
+'''########################################################'''
+
+
+'''Hands On'''
+# Take donut count from the user and print a string of the 
+# form 'Number of donuts: <count>'
+# where <count> is the donut count entered by the user. 
+# However, if the count is 10 or more, then use the word 'many'
+# instead of the actual count.
+# Number of donuts: 5
+# Number of donuts: many
+
+
+'''Hands On'''
+# Take input string from the user, 
+# print a string made of the first 2
+# and the last 2 chars of the original string,
+# so 'spring' yields 'spng'. However, if the string length
+# is less than 2, return instead the empty string.
+
+
+'''Hands On'''
+# Take the age as input from the user and print whether he is a infant, Child , 
+# Adult,  Senior Citizen
+# 0 - 1 is Infant
+# > 1 and < than 18 is Child 
+# > 18 and < 60 is Adult
+# > 60 is Senior Citizen 
+
+
+'''Hands On'''
+# Given a string, if its length is at least 3,
+# add 'ing' to its end.
+# Unless it already ends in 'ing', in which case
+# add 'ly' instead.
+# If the string length is less than 3, leave it unchanged.
+# Print the resulting string.
+
+
+'''Hands On'''
+# Consider dividing a string into two halves.
+# If the length is even, the front and back halves are the same length.
+# If the length is odd, we'll say that the extra char goes in the front half.
+# e.g. 'abcde', the front half is 'abc', the back half 'de'.
+# Given 2 strings, a and b, return a string of the form
+#  a-front + b-front + a-back + b-back
+
+
+
+'''Hands On'''
+# Print all the numbers from 1 to 10 using condition in while loop
+
+'''Hands On'''
+# Print all the numbers from 1 to 10 using while True loop
+
+'''Hands On'''
+# Print all the even numbers from 1 to 10 using condition in while loop
+
+'''Hands On'''
+# Print all the even numbers from 1 to 10 using while True loop
+
+'''Hands On'''
+# Print all the odd numbers from 1 to 10 using condition in while loop
+
+'''Hands On'''
+# Print all the odd numbers from 1 to 10 using while True loop
+
+
+"""
+Code Challenge
+  Name: 
+    Fizz Buzz
+  Filename: 
+    fizzbuzz.py
+  Problem Statement:
+    Write a Python program which iterates the integers from 1 to 50(included). 
+    For multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
+    For numbers which are multiples of both three and five print "FizzBuzz". 
+    User Input not required  
+  Output:
+    1
+    2
+    Fizz
+    4 
+    Buzz  
+"""
