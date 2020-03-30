aa91be8586b60866cdcd0a3a3623cd59f09beac4
mohitsharma2 <mohit121993@gmail.com>
2020-03-30 22:23:42 +0530

    Added new file

---
+++ xman.py
@@ -0,0 +1,30 @@
+"""
+Analysis of Salaries Data ( Hand On Activity )
+
+1. Which Male and Female Professor has the highest and the lowest salaries
+2. Which Professor takes the highest and lowest salaries.
+3. Missing Salaries - should be mean of the matching salaries of those 
+   whose service is the same
+4. Missing phd - should be mean of the matching service 
+5. How many are Male Staff and how many are Female Staff. 
+   Show both in numbers and Graphically using Pie Chart.  
+   Show both numbers and in percentage
+6. How many are Prof, AssocProf and AsstProf. 
+   Show both in numbers adn Graphically using a Pie Chart
+7. Who are the senior and junior most employees in the organization.
+8. Draw a histogram of the salaries divided into bin starting 
+   from 50K and increment of 15K
+"""
+
+import pandas as pd
+
+df = pd.read_csv("C:/Users/mohit/Desktop/FORSKCODE/Salaries.csv")
+
+
+
+
+
+
+
+
+
