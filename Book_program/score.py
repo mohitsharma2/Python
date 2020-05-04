9f4ed05f1c48d8097a51fce8a375ab7186d64632
mohitsharma2 <mohit121993@gmail.com>
2020-05-04 23:11:12 +0530

    Added new file

---
+++ Book_program/score.py
@@ -0,0 +1,112 @@
+print("Select the correct option from (a,b,c,d)")
+
+print("CURRENT AFFAIRS")
+
+print("(1) 'TB Harega Desh Jeetega' campaign aims to end Tuberculosis by the year ?\n(a)2025 (b)2030 (c)2035 (d) 2020 :")
+answer=input("Enter your answer a,b,c,d:")
+score=0
+answer1 = "Correct"
+answer2 = "Wrong"
+
+if(answer == 'a'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(2) Vivad Se Vishwas scheme is related to ? \n(a) Direct taxes (b)Indirect taxes(c) Both (A) and (B) (d) Neither (A) nor (B) ") 
+answer=input("Enter your answer a,b,c,d :")
+
+if(answer == 'a'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("Question :3 The Concept of Thalinomics has been presented in India in ?\n(a)The Union Budget 2020-21 (b) Economic Survey 2019-20 (c)SDG India Index 2019 (d)National Financial Inclusion Strategy 2019-24 ")
+answer=input("Enter your answer a,b,c,d :")
+
+if(answer == 'b'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(4)Who is the current Finance Minister? \n(a)Kemi Adeosun (b)Zainab Ahmed (c)Aisha Bello (d)Zainab Adamu : ")
+sol4=input("Enter your answer a,b,c,d:")
+
+if(sol4 == 'b'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(5)One of this was in existence before the outbreak of the second World War? \n(a)The league of Nations (b)The UNO (c)The Commonwealth of Nations (d)ECOWAS:  ")
+sol5=input("Enter your answer a,b,c,d:")
+
+if(sol5 == 'a'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(6)Nigeria got her independence in what year? \n(a)1970 (b)1960 (c)1980 (d)1950: ")
+sol6=input("Enter your answer a,b,c,d:")
+score=0
+answer1 = "Correct"
+answer2 = "Wrong"
+
+if(sol6 == 'b'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(7)Which state in Nigeria has the highest Local Goverment Area? \n(a)Lagos (b)Niger (c)Kano (d)Edo: ")
+sol7=input("Enter your answer a,b,c,d:")
+
+if(sol7 == 'c'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(8)Which state in Nigeria is the food basket of the nation? \n(a)Benue (b)Enugu (c)Kaduna (d)Ogun: ")
+sol8=input("Enter your answer a,b,c,d:")
+
+if(sol8 == 'a'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+
+print("(9)A bill that applies to the whole population and is intended to promote the general welfare is called? \n(a)A private bill (b)A decree (c)An Appropriation bill (d)A public bill: ")
+sol9=input("Enter your answer a,b,c,d:")
+
+if(sol9 == 'c'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+print("(10)The '2019 Nobel Prize for Medicine' has been awarded to 'William Kaelin', Gregg Semenza' and Peter Ratcliffe' for their discoveries of -
+A novel therapy against malaria
+How cell sense and adapt to oxygen availability\n(a)Molecular mechanisms controlling the circadian rhythm (b)Mechanisms for Autophagy
+ (c)Gen. Thomas Aguiyi-Ironsi (d)Chief Olusegun Obasanjo:  ")
+sol10=input("Enter your answer a,b,c,d:")
+
+if(sol10 == 'c'):
+    print(answer1)
+    score=score + 2
+else:
+   print(answer2)
+   score=score - 2
+print(score)
